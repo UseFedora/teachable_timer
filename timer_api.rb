1b0c24f179dec53816fb573c11e2d74468d8ee3d
Noah Pryor <noah@noahpryor.com>
2016-06-15 14:43:53 -0400

    timer api endpoint

--- timer_api.rb
+++ timer_api.rb
@@ -8,11 +8,13 @@ require_relative 'generate_gif'
 
 $pwd = ENV['PWD']
 
-get '/api/timer/end_time=:timestamp' do |filename|
+get '/api/timer.gif' do
+  timestamp = request.env["rack.request.query_hash"]["end_time"]
   # generate file for timestamp and store local dir
-  # filename = combine_images(timestamp)
-  timestamp = params[:timestamp]
-  puts timestamp
-  filename = "hours/23.gif"
-  send_file("./tmp/frames/#{filename}", filename: filename, type: 'image/gif', disposition: :inline)
+  start_time = Time.now.to_i
+  end_time = timestamp.to_i
+  output_path = "./tmp/#{start_time}-#{end_time}.gif"
+  create_gif(start_time: start_time, end_time: end_time, output_path: output_path)
+  send_file(output_path, filename: output_path, type: 'image/gif', disposition: :inline)
 end
+
