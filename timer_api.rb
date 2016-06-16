0ee7cbd864b43cdcf61bf86724d6da879e12fd2a
Noah Pryor <noah@noahpryor.com>
2016-06-16 15:20:14 -0400

    use MS since epoch

--- timer_api.rb
+++ timer_api.rb
@@ -9,10 +9,10 @@ require_relative 'lib/gif_timer'
 $pwd = ENV['PWD']
 
 get '/api/timer.gif' do
-  timestamp = request.env["rack.request.query_hash"]["end_time"]
+  end_time_ms_since_epoch = request.env["rack.request.query_hash"]["end_time"].to_i
   # generate file for timestamp and store local dir
   start_time = Time.now.to_i
-  end_time = timestamp.to_i
+  end_time = end_time_ms_since_epoch / 1000
   time_difference =  end_time - start_time
   if start_time > end_time
     time_difference = 0
